b27a7c64ae27a57b8c0da00c32bf1df3888752d4
Jacqueline Wijaya <jackiewijaya@webmocha.com>
2016-08-01 16:11:37 -1000

    updating TilesDoc to match updates to grommet Tiles PR
    Signed-off-by: Jackie Wijaya <jackiewijaya@webmocha.com>

--- src/develop/components/TilesDoc.js
+++ src/develop/components/TilesDoc.js
@@ -139,12 +139,12 @@ class TileDoc extends Component {
             <dt><code>size        small|medium|large</code></dt>
             <dd>The width of the contained tiles. Defaults to <code>medium</code>.</dd>
             <dt><code>masonry     true|false</code></dt>
-            <dd>Whether or not to update number of columns based on component width.
-              Defaults to <code>false</code>. Can set number of columns using
-              `numColumns`.</dd>
+            <dd>Whether to update the number of columns based on the component width.
+              Defaults to <code>false</code>. The number of columns can be set with
+              <code>numColumns</code>.</dd>
             <dt><code>numColumns  number</code></dt>
             <dd>Number of columns to allow for masonry option, based on component width.
-              Responds based on minimum column width.</dd>
+              Responds based on the width of the tile children.</dd>
           </dl>
           <p>Options for <Link to={this.context.routePrefix + "box"}>Box</Link> are
           also available for Tiles.</p>
@@ -246,7 +246,7 @@ class TileDoc extends Component {
             </Tiles>
           </div>
 
-          <h3>Large, Masonry, 4-Columns</h3>
+          <h3>Masonry, 4-Columns</h3>
           <div className="example">
             <Tiles masonry={true} numColumns={4}>
               {this._renderRichTiles({alternateContent: true})}
